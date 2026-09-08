d45c8414b0a186aa90bf7150b853f8f63a0ae59d
Lars Ingebrigtsen <larsi@gnus.org>
2026-09-08 14:38:49 +0200

    Scaffolding

---
+++ quick-calendar.el
@@ -0,0 +1,22 @@
+;;; quick-calendar.el --- Adding entries to Google Calendar -*- lexical-binding: t; -*-
+;; Copyright (C) 2026 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+
+;; quick-calendar.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 2, or (at your
+;; option) any later version.
+
+;;; Commentary:
+
+
+;;; Code:
+
+(require 'cl-lib)
+
+
+
+(provide 'quick-calendar)
+
+;;; quick-calendar.el ends here.
